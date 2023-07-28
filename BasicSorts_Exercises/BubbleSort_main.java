36b6718b00478571027b802bbeebebed33f97f99
bektsahmt <21010310003@ogrenci.bartin.edu.tr>
2023-07-28 11:49:33 +0300

    BasicSorts LeetCode Exercises

---
+++ BasicSorts_Exercises/BubbleSort_main.java
@@ -0,0 +1,26 @@
+package BasicSorts_Exercises;
+
+public class BubbleSort_main {
+
+	public static void main(String[] args) {
+		
+		 LinkedList myLinkedList = new LinkedList(4);
+	        myLinkedList.append(2);
+	        myLinkedList.append(6);
+	        myLinkedList.append(5);
+	        myLinkedList.append(1);
+	        myLinkedList.append(3);
+
+
+
+	        System.out.println("Unsorted Linked List:");
+	        myLinkedList.printList();
+
+	        myLinkedList.bubbleSort();
+
+	        System.out.println("\nSorted Linked List:");
+	        myLinkedList.printList();
+
+	}
+
+}
